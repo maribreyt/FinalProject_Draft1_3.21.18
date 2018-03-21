5d8c230446b11832cf0e786aa2ce106082801eda
maribreyt <mb4144@tc.columbia.edu>
2018-03-21 17:24:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/finalproject_v2/Rewards.java
@@ -0,0 +1,22 @@
+package com.example.android.finalproject_v2;
+
+//import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by maribreyt on 3/21/18.
+ */
+
+public class Rewards extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.rewards_tab3, container, false);
+    }
+}
